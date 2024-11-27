16f84e728308857cd8ff6a8c951fc774f2d9b29e
Luiza Avelino <avelino.luiza98@gmail.com>
2024-11-27 20:47:57 -0300

    add: get file from terminal

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,17 @@
+import java.io.File;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter filename with extension: ");
+
+        String filename = scanner.next();
+        File file = new File("public"+"/"+filename);
+        if(file.exists()) {
+            System.out.println("arquivo existe");
+        } else {
+            System.out.println("arquivo nao existe");
+        }
     }
 }
